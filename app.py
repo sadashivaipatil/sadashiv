c3ef0da38ad3277c2d12f42f6ddbf99e4de2cf8f
Sadashiva <sadashivaipatil35@gmail.com>
2026-06-06 21:01:27 +0530

    first commit

--- app.py
+++ app.py
@@ -1,6 +1,34 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
+
 app = Flask(__name__)
+
 @app.route("/")
 def home():
     return render_template("index.html")
-app.run()
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/skills")
+def skills():
+    return render_template("skills.html")
+
+@app.route("/languages")
+def languages():
+    return render_template("languages.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/volunteering")
+def volunteering():
+    return render_template("volunteering.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
