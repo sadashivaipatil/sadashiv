953a2429580abcbd36cb234f4141d7b8757a8977
Sadashiva <sadashivaipatil35@gmail.com>
2026-06-08 03:52:09 +0530

    first commit

--- static/script.js
+++ static/script.js
@@ -496,3 +496,37 @@ tiltCards.forEach(card=>{
     });
 
 });
+// FLOATING PARTICLES
+
+const particlesContainer =
+document.getElementById("particles");
+
+if(particlesContainer){
+
+    for(let i=0;i<40;i++){
+
+        const particle =
+        document.createElement("div");
+
+        particle.classList.add("particle");
+
+        particle.style.left =
+        Math.random()*100 + "%";
+
+        particle.style.width =
+        Math.random()*5 + 2 + "px";
+
+        particle.style.height =
+        particle.style.width;
+
+        particle.style.animationDuration =
+        Math.random()*15 + 10 + "s";
+
+        particle.style.animationDelay =
+        Math.random()*5 + "s";
+
+        particlesContainer.appendChild(
+            particle
+        );
+    }
+}
